7080ef40724c4695f6283a89b3333413c0450028
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2020-09-01 22:00:46 +0100

    Add w, X, and closest()

--- bioslam/main.py
+++ bioslam/main.py
@@ -19,11 +19,75 @@ from geometry_msgs.msg import Point, Twist, Vector3
 from gazebo_msgs.msg import LinkStates
 from std_msgs.msg import String
 
+# VERSION 1
+
+# Input: 2 inputs, 1 for each of the nearest cones on the left and right
+# Output: A 2x2 grid
+
+# Due to the small size of the output layer, it is impractical to implement a true
+# Kohonen network, which also updates the weights of the units in the winner's neighbourhood.
+# Therefore only the weights of the winner will be updated.
+
+# Declare constants
+DIM_X = 2 # Dimensionality of X input vectors
+LA = 0.3    # λ coefficient
+DLA = 0.05  # Δλ
+
+def distance(w, x):
+    r = 0
+    for i in range(len(w)):
+        r = r + (w[i] - x[i])*(w[i] - x[i])
+    
+    r = np.sqrt(r)
+    return r
+
+def closest(cones):
+    """
+    Returns the distances of the closest right and left cones, respectively
+    
+    :param cones: An array of x-y cone positions, relative to vehicle
+    :return:
+        - right: The distance to the closest cone on the right
+        - left: The distance to the closest cone on the left
+    """
+    right_cones = cones
+    left_cones = np.zeros(0, len(cones[0]))
+    right = None
+    left = None
+
+    for i in range(len(cones)):
+        if (cones[i, 0] < 0):
+            left_cones = np.vstack((left_cones, cones[i]))
+            right_cones = np.delete(right_cones, i, 0)
+
+    right = math.hypot(right_cones[0, 0], right_cones[0, 1])
+    for j in range(len(right_cones)):
+        d = math.hypot(right_cones[j, 0], right_cones[j, 1])
+        if (d < right):
+            right = d
+
+    left = math.hypot(left_cones[0, 0], left_cones[0, 1])
+    for k in range(len(left_cones)):
+        d = math.hypot(left_cones[k, 0], left_cones[k, 1])
+        if (d < left):
+            left = d
+
+    return right, left
+
 
 class Listener(BaseListener):
 
     def __init__(self):
         super().__init__('bioslam')
+
+        # Initialize weights
+        self.w = np.random.rand(2, 2)
+        norm = np.linalg.norm(self.w)
+        self.w /= norm # Normalise
+
+        # Initialize inputs
+        self.capture = []
+        self.X = []
 
         # Set publishers
         self.map_pub = self.create_publisher(ConeArray, '/mapping/map', 10)
@@ -40,6 +104,8 @@ class Listener(BaseListener):
         # Place x y positions of cones into self.capture
         self.capture = np.array([[cone.x, cone.y] for cone in msg.cones])
         print(self.capture)
+        right, left = closest(self.capture)
+        self.X = np.array([right, left])
 
     def control_callback(self, msg: Twist):
         str(msg) # For some reason this is needed to access msg.linear.x
