baf649c56add5ddd19f0b28151859af77da67680
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2020-09-15 15:24:29 +0100

    Move closest() to scratch.py

--- bioslam/main.py
+++ bioslam/main.py
@@ -41,40 +41,6 @@ def distance(w, x):
     r = np.sqrt(r)
     return r
 
-def closest(cones):
-    """
-    Returns the distances of the closest right and left cones, respectively
-    
-    :param cones: An array of x-y cone positions, relative to vehicle
-    :return:
-        - right: The distance to the closest cone on the right
-        - left: The distance to the closest cone on the left
-    """
-    right_cones = cones
-    left_cones = np.zeros(0, len(cones[0]))
-    right = None
-    left = None
-
-    for i in range(len(cones)):
-        if (cones[i, 0] < 0):
-            left_cones = np.vstack((left_cones, cones[i]))
-            right_cones = np.delete(right_cones, i, 0)
-
-    right = math.hypot(right_cones[0, 0], right_cones[0, 1])
-    for j in range(len(right_cones)):
-        d = math.hypot(right_cones[j, 0], right_cones[j, 1])
-        if (d < right):
-            right = d
-
-    left = math.hypot(left_cones[0, 0], left_cones[0, 1])
-    for k in range(len(left_cones)):
-        d = math.hypot(left_cones[k, 0], left_cones[k, 1])
-        if (d < left):
-            left = d
-
-    return right, left
-
-
 class Listener(BaseListener):
 
     def __init__(self):
@@ -104,8 +70,6 @@ class Listener(BaseListener):
         # Place x y positions of cones into self.capture
         self.capture = np.array([[cone.x, cone.y] for cone in msg.cones])
         print(self.capture)
-        right, left = closest(self.capture)
-        self.X = np.array([right, left])
 
     def control_callback(self, msg: Twist):
         str(msg) # For some reason this is needed to access msg.linear.x
