632417b18e88fe3094393e873c093b07b8c214af
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2020-09-16 20:00:22 +0100

    Update functions from Fun w SOM

--- bioslam/main.py
+++ bioslam/main.py
@@ -23,113 +23,76 @@ from gazebo_msgs.msg import LinkStates
 # Declare constants
 IN_MAX = 30 # Max input amount (i.e. max number of cones at once)
 IN_VECT = 2 # The number of input vectors
-X_OUT = 100 # X size of output layer
-Y_OUT = 100 # Y size of output layer
-MAP_R = max(X_OUT, Y_OUT)/2 # Radius of map at t0
-ITER = 1000 # Number of iterations
-TIME_CONST = ITER / np.log(MAP_R)
-LAM = 0.3    # λ coefficient
-DLAM = 0.05  # Δλ
+X_OUT = 20 # X size of output layer
+Y_OUT = 20 # Y size of output layer
+SIGMA0 = max(X_OUT, Y_OUT)/2 # Radius of map at t0
+LAM = 500 # Lambda, the time scaling constant
+L0 = 0.3 # Initial learning rate
 
 class Listener(BaseListener):
 
     def __init__(self):
         super().__init__('bioslam')
 
-        # Initialise some member variables
         self.t = 0 # Timestep (iteration count)
-        self.lam = LAM # The learning rate, λ coefficient
-        self.n_rad = MAP_R # Neighbourhood radius
 
         # Initialise arrays
         self.coords = np.indices((X_OUT, Y_OUT))
         self.coords = np.flip(self.coords.T, axis=2).reshape((X_OUT * Y_OUT, 2))
         self.diff = None # Difference between inputs and weights (capture - W)
-        self.distances = None # Array of distances to the BMU
+        self.bmu_dists = []
 
         # Initialize weights
         self.w = np.random.rand(X_OUT, Y_OUT, IN_MAX, IN_VECT)
         norm = np.linalg.norm(self.w)
         self.w /= norm # Normalise (? ask Alex)
 
-        # Initialize inputs
-        self.capture = []
-        self.X = []
-
         # Set publishers
         self.map_pub = self.create_publisher(ConeArray, '/mapping/map', 10)
         self.pose_pub = self.create_publisher(CarPos, '/mapping/position', 10)
-        self.cmd_pub = self.create_publisher(Twist, '/gazebo/cmd_vel', 10)
 
         # Set subscribers
         self.cones_sub = self.create_subscription(ConeArray, '/cones/positions', self.cones_callback, 10)
-        self.gnss_sub = self.create_subscription(NavSatFix, '/peak_gps/gps', self.gnss_callback, 10)
-        self.imu_sub = self.create_subscription(IMU, '/peak_gps/imu', self.imu_callback, 10)
-        self.control_sub = self.create_subscription(Twist, '/gazebo/cmd_vel', self.control_callback, 10)
     
     def compute_bmu_distances(self, bmu):
         diff = bmu - self.coords
         d_sq = diff[:, 0]**2 + diff[:, 1]**2
         d = np.sqrt(d_sq).reshape((X_OUT, Y_OUT))
 
-        self.distances = d
-
-    def compute_influence(self):
-        theta = np.exp(-(self.distances**2)/(2 * self.n_rad**2))
-
-        return theta
-
-    def compute_lambda(self):
-        self.lam = LAM * np.exp(-self.t/ITER)
-
-    def compute_radius(self):
-        self.n_rad = MAP_R * np.exp(-self.t/TIME_CONST)
-
-    def compute_weights(self, nbh):
-        old_weights = self.w[nbh[:, 0], nbh[:, 1]]
-        theta = self.compute_influence()[nbh[:, 0], nbh[:, 1]].reshape(len(old_weights[:]), 1, 1)
-        self.w[nbh[:, 0], nbh[:, 1]] = old_weights + theta * self.lam * self.diff[nbh[:, 0], nbh[:, 1]]
+        return d
 
     def cones_callback(self, msg: ConeArray):
-        if (self.n_rad < 1):
+        t = self.t
+        if (self.sigma(t) < 1):
             return
-        # Place x y positions of cones into self.capture
-        self.capture = np.array([[cone.x, cone.y] for cone in msg.cones])
+        # Place x y positions of cones into input array x
+        x = np.array([[cone.x, cone.y] for cone in msg.cones])
         # Pad capture array with zeros for give it a shape of (IN_MAX, IN_VECT)
-        self.capture = np.vstack((self.capture, np.zeros((30 - len(self.capture[:, 0]), IN_VECT))))
+        x = np.vstack((x, np.zeros((30 - len(x[:, 0]), IN_VECT))))
         a = time.time()
-        bmu = self.get_bmu() # Determine BMU coordinates
+        bmu = self.get_bmu(x) # Determine BMU coordinates
         b = time.time()
-        self.compute_bmu_distances(bmu) # Calculate distance from each node to BMU
+        dist = self.compute_bmu_distances(bmu) # Calculate distance from each node to BMU
         c = time.time()
-        nbh = np.argwhere(self.distances <= self.n_rad) # Get indices of neighbourhood
+        self.update(dist, t) # Compute weights of neighbourhood nodes
         d = time.time()
-        self.compute_weights(nbh) # Compute weights of neighbourhood nodes
-        e = time.time()
         print('get bmu: ' + str(b-a))
         print('compute dist: ' + str(c-b))
-        print('get nbh: ' + str(d-c))
-        print('compute weight: ' + str(e-d))
-        print('t: ' + str(self.t), '\nlambda: ' + str(self.lam), '\nrad: ' + str(self.n_rad))
+        print('compute weight: ' + str(d-c))
+        print('t: ' + str(t), '\nlambda: ' + str(self.L(t)), '\nrad: ' + str(self.sigma(t)))
         self.t += 1 # Increment timestep
-        self.compute_lambda()
-        self.compute_radius()
-    def control_callback(self, msg: Twist):
-        str(msg) # For some reason this is needed to access msg.linear.x
-        self.v = msg.linear.x
-        self.yaw = msg.angular.z
-        self.u = np.array([self.v, self.yaw]).reshape(2, 1)
+        print(bmu)
+        print('Quantisation error: ' + str(self.quant_err()))
+        # self.plot_som()
 
-        self.get_logger().info(f'Command confirmed: {msg.linear.x} m/s turning at {msg.angular.z} rad/s')
-
-    def get_bmu(self):
+    def get_bmu(self, x):
         """
         Determines the X-Y position of the winning node in the output layer
         """
         # Make self.capture 4D
         j = time.time()
         cap = np.zeros((1, 1, IN_MAX, IN_VECT))
-        cap += self.capture
+        cap += x
         k = time.time()
         self.diff = cap - self.w # Calculate differences
         l = time.time()
@@ -139,6 +102,7 @@ class Listener(BaseListener):
         # Get X-Y position of the node with the minimum distance
         bmu = np.unravel_index(np.argmin(results, axis = None), results.shape)
         n = time.time()
+        self.bmu_dists.append(np.linalg.norm(self.diff[bmu]))
         print('make 4d: ' + str(k-j))
         print('diff: ' + str(l-k))
         print('results: ' + str(m-l))
@@ -146,32 +110,41 @@ class Listener(BaseListener):
 
         return bmu
 
-    def gnss_callback(self, msg: NavSatFix()):
-        # Log data retrieval
-        self.get_logger().info(f'From GNSS: {msg.latitude}, {msg.longitude}')
+    def get_x_y(self):
+        x = self.w[:, :, :, 0].flatten()
+        y = self.w[:, :, :, 1].flatten()
+
+        return x, y
+
+    def L(self, t):
+        return L0 * np.exp(-t/LAM)
+
+    def N(self, dist, t):
+        """
+        Computes the neighbouring penalty N
+
+        :param dist: An array of distances to the BMU, matching the shape of the output layer
+        :param t: The current timestep
+        """
+        sigma_t = self.sigma(t)
+        return np.exp(-(dist**2)/(2 * sigma_t**2))
+
+    def plot_som(self):
+        x, y = self.get_x_y()
+        plt.scatter(x, y, marker='.', c='k')
+        plt.show()
+        plt.pause(0.0001)
+        plt.clf()
     
-    def imu_callback(self, msg: IMU()):
-        # Log data retrieval
-        self.get_logger().info(f'From IMU: {msg.longitudinal}, {msg.lateral}, {msg.vertical}')
+    def quant_err(self):
+        return np.array(self.bmu_dists).mean()
+    
+    def sigma(self, t):
+        return SIGMA0 * np.exp(-t/LAM)
 
-    def link_states_callback(self, links_msg: LinkStates):
-        cones = []
-        for name, pose in zip(links_msg.name, links_msg.pose):
-            if 'blue_cone' in name:
-                label = Label.BLUE_CONE
-            elif 'yellow_cone' in name:
-                label = Label.YELLOW_CONE
-            elif 'big_orange_cone' in name:
-                label = Label.BIG_ORANGE_CONE
-            elif 'orange_cone' in name:
-                label = Label.ORANGE_CONE
-            else:
-                # if not a cone
-                continue
-            cones.append(Cone(position=pose.position, label=Label(label=label)))
-
-    def timer_callback(self):
-        print('timer_callback()')
+    def update(self, dist, t):
+        Ndt = self.N(dist, t).reshape((X_OUT, Y_OUT, 1, 1))
+        self.w += Ndt * self.L(t) * self.diff
 
 def main(args=None):
     rclpy.init(args=args)
